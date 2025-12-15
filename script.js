f3db8080ce76bec58d5bbd380e47ec07e2b6ee69
Maxim Zhzhenov <125403308+Clarity-afk@users.noreply.github.com>
2025-12-16 00:31:22 +0300

    forgot what i did there

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function toggleLike(button) {
-    const likeImage = button.querySelector('img');  // Находим картинку внутри кнопки
+  const likeImage = button.querySelector("img");
 
-    // Проверяем текущее изображение и меняем на активный или обычный лайк
-    if (likeImage.src.includes('images/icons/like.svg')) {
-        likeImage.src = 'images/icons/activelike.svg';
-    } else {
-        likeImage.src = 'images/icons/like.svg';
-    }
+  if (likeImage.src.includes("images/icons/like.svg")) {
+    likeImage.src = "images/icons/activelike.svg";
+  } else {
+    likeImage.src = "images/icons/like.svg";
+  }
 }
+
+const buttonToLoad = document.querySelector(".loader");
+const loadMore = document.querySelector(".tools__btn button");
+
+loadMore.addEventListener("click", () => {
+  buttonToLoad.classList.remove("unactive");
+  loadMore.remove();
+});
